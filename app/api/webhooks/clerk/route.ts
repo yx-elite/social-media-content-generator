480c4dab8f80bc8e1e4b370439df508848c7f17a
Yi Xian <exian0716@gmail.com>
2024-12-08 21:26:23 +0800

    ✨ feat: Sync Clerk authentication with Neon DB

---
+++ app/api/webhooks/clerk/route.ts
@@ -0,0 +1,78 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { createOrUpdateUser } from "@/utils/db/action";
+
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      "Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local",
+    );
+  }
+
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET);
+
+  // Get the headers
+  const headerPayload = await headers();
+  const svixId = headerPayload.get("svix-id");
+  const svixTimestamp = headerPayload.get("svix-timestamp");
+  const svixSignature = headerPayload.get("svix-signature");
+
+  // Return 400 error if svix headers are missing
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response("Error: Request could not be verified", {
+      status: 400,
+    });
+  }
+
+  // Get the request body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  let evt: WebhookEvent;
+
+  // Verify payload/body with headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svixId,
+      "svix-timestamp": svixTimestamp,
+      "svix-signature": svixSignature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification failed", { status: 400 });
+  }
+
+  // TODO: Save user to database
+  if (evt.type === "user.created" || evt.type === "user.updated") {
+    const { id, email_addresses, first_name, last_name } = evt.data;
+    const email = email_addresses[0]?.email_address;
+    const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+    if (email) {
+      try {
+        await createOrUpdateUser(id, email, name);
+        return new Response("User created or updated", { status: 200 });
+      } catch (err) {
+        console.error("Error: Failed to create or update user:", err);
+        return new Response("Error processing user data", {
+          status: 500,
+        });
+      }
+    }
+  }
+  // TODO: Send welcome email
+
+  // // Sample: Log payload to console
+  // const { id } = evt.data; 
+  // const eventType = evt.type;
+  // console.log(
+  //   `Received webhook with ID ${id} and event type of ${eventType}`,
+  // );
+  // console.log("Webhook payload:", body);
+
+  // return new Response("Webhook received", { status: 200 });
+}
---
+++ utils/db/action.ts
@@ -0,0 +1,41 @@
+import { db } from "@/utils/db";
+import { Users } from "@/utils/db/schema";
+import { eq } from "drizzle-orm";
+
+export async function createOrUpdateUser(
+  clerkUserId: string,
+  email: string,
+  name: string,
+) {
+  try {
+    const [existingUser] = await db
+      .select()
+      .from(Users)
+      .where(eq(Users.stripeCustomerId, clerkUserId))
+      .limit(1)
+      .execute();
+
+    if (existingUser) {
+      const [updatedUser] = await db
+        .update(Users)
+        .set({ email, name })
+        .where(eq(Users.stripeCustomerId, clerkUserId))
+        .returning()
+        .execute();
+      console.log("Updated user", updatedUser);
+      return updatedUser;
+    }
+
+    const [newUser] = await db
+      .insert(Users)
+      .values({ email, name, stripeCustomerId: clerkUserId, points: 150 })
+      .returning()
+      .execute();
+    console.log("Created new user", newUser);
+    return newUser;
+
+  } catch (err) {
+    console.error("Error creating or updating user:", err);
+    return null;
+  }
+}
